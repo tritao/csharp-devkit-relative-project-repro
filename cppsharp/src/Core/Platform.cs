119a4581d3a8df6274323261a603656c91b22475
Joao Matos <joao@tritao.eu>
2024-09-26 19:59:40 +0100

    add

---
+++ cppsharp/src/Core/Platform.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CppSharp
+{
+    public enum TargetPlatform
+    {
+        Windows,
+        Linux,
+        Android,
+        MacOS,
+        iOS,
+        WatchOS,
+        TVOS,
+        Emscripten,
+    }
+
+    public static class Platform
+    {
+        public static bool IsWindows
+        {
+            get
+            {
+                switch (Environment.OSVersion.Platform)
+                {
+                    case PlatformID.Win32NT:
+                    case PlatformID.Win32S:
+                    case PlatformID.Win32Windows:
+                    case PlatformID.WinCE:
+                        return true;
+                }
+
+                return false;
+            }
+        }
+
+        public static bool IsLinux
+        {
+            get { return Environment.OSVersion.Platform == PlatformID.Unix; }
+        }
+
+        public static bool IsMono
+        {
+            get { return Type.GetType("Mono.Runtime") != null; }
+        }
+
+        public static bool IsUnixPlatform
+        {
+            get
+            {
+                var platform = Environment.OSVersion.Platform;
+                return platform == PlatformID.Unix || platform == PlatformID.MacOSX;
+            }
+        }
+
+        public static TargetPlatform Host
+        {
+            get
+            {
+                if (IsWindows)
+                    return TargetPlatform.Windows;
+
+                if (IsLinux)
+                    return TargetPlatform.Linux;
+
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ proj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Example
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CppSharp.Platform.IsWindows);
+        }
+    }
+}
